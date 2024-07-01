be2004977528db4047d2b76ee4cd53731b055c5f
seyoung34 <jupi0304@naver.com>
2024-07-01 15:21:16 +0900

    검색창, 삭제버튼, 리스트뷰 xml생성

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -1,11 +1,19 @@
 package campus.tech.kakao.map
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val search = findViewById<EditText>(R.id.search)
+        val close = findViewById<ImageView>(R.id.close_icon)
+        val listView = findViewById<ListView>(R.id.list_view)
+
     }
 }
