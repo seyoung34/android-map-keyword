f31b16e7cbd228dc2f68b85d7b424ed2a0f67cbe
seyoung34 <jupi0304@naver.com>
2024-07-05 12:44:22 +0900

    리사이클러뷰로 변경 완료

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -1,12 +1,15 @@
 package campus.tech.kakao.map
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,11 +18,16 @@ class MainActivity : AppCompatActivity() {
 
         val search = findViewById<EditText>(R.id.search)
         val close = findViewById<ImageView>(R.id.close_icon)
-        val listView = findViewById<ListView>(R.id.list_view)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val dataGeneration = findViewById<TextView>(R.id.data_generation)
-        val refresh = findViewById<TextView>(R.id.refresh)
+        val searchBotton = findViewById<TextView>(R.id.searchBotton)
         val delete = findViewById<TextView>(R.id.data_delete)
+        val recentSearch = findViewById<RecyclerView>(R.id.recent_search)
+
         val dbManager = DatabaseManager(context = this)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recentSearch.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false)    //마지막 인자는 역순으로 배치 여부
 
         dataGeneration.setOnClickListener{
             with(dbManager){
@@ -31,14 +39,15 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        refresh.setOnClickListener {
+        searchBotton.setOnClickListener {
             val query = search.text.toString()
             if (query.isEmpty()) {
                 Toast.makeText(this, "검색어를 입력하세요", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             val dummy = dbManager.searchPlacesKind(query)
-            listView.adapter = PlaceAdapter(dummy)
+            recyclerView.adapter = PlaceAdapter(dummy)
+            Log.d("testt", dummy.toString())
         }
 
         delete.setOnClickListener{
--- app/src/main/java/campus/tech/kakao/map/PlaceAdapter.kt
+++ app/src/main/java/campus/tech/kakao/map/PlaceAdapter.kt
@@ -1,40 +1,40 @@
 package campus.tech.kakao.map
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
-class PlaceAdapter(private val items : List<Place>) : BaseAdapter() {
-    override fun getCount(): Int {
-        return items.size
-    }
+class PlaceAdapter(private val items : List<Place>) : RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
 
-    override fun getItem(position: Int): Place {
-        return items[position]
-    }
-
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
-    }
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view: View =
-            LayoutInflater.from(parent?.context).inflate(R.layout.item_listview, parent, false)
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
 
         val id : TextView = view.findViewById(R.id.id)
         val name: TextView = view.findViewById(R.id.name)
         val address: TextView = view.findViewById(R.id.address)
         val kind: TextView = view.findViewById(R.id.kind)
+    }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View =
+            LayoutInflater.from(parent?.context).inflate(R.layout.item_recyclerview, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
-        id.text = item.id.toString()
-        name.text = item.name
-        address.text = item.address
-        kind.text = item.kind
+        with(holder) {
+            id.text = item.id.toString()
+            name.text = item.name
+            address.text = item.address
+            kind.text = item.kind
+            Log.d("Testt",holder.name.text.toString())
+        }
+    }
 
-        return view
-
+    override fun getItemCount(): Int {
+        return items.size
     }
 }
